60a0158a77be86de97b9d9d472cbfe44b1bceef1
Honza Dudek <honza@honzadudek.cz>
2018-09-20 11:20:50 +0200

    First 4 excercises

---
+++ week-01/day-04/src/Excercise03.java
@@ -0,0 +1,7 @@
+public class Excercise03 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Marek!");
+        System.out.println("Hello, Tomek!");
+        System.out.println("Hello, Ivanka!");
+    }
+}
---
+++ week-01/day-04/src/Excercise04.java
@@ -0,0 +1,10 @@
+public class Excercise04 {
+    public static void main(String[] args) {
+        String name = "Honza Dudek";
+        int age = 27;
+        double height = 1.85;
+        System.out.println("Hello, my name is: " + name + ".");
+        System.out.println("I am " + age + " years old.");
+        System.out.println("I am " + height + " meters tall.");
+    }
+}
