454531afbad5f80eeb2acbf1a1135a5424b4c76e
Honza Dudek <honza@honzadudek.cz>
2018-09-20 11:53:23 +0200

    6th excercise

--- week-01/day-04/src/Excercise06.java
+++ week-01/day-04/src/Excercise06.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Excercise06 {
+    public static void main(String[] args) {
+        int dailyCodingHours = 6;
+        int weeksInSemester = 17;
+        int workingHoursInWeek = 52;
+
+        int hoursSpendCodingInSemester = dailyCodingHours * 5 * weeksInSemester;
+        double percentageOfCodingTime = (double)hoursSpendCodingInSemester / (workingHoursInWeek * weeksInSemester) * 100;
+
+        System.out.println("Student codes for " + hoursSpendCodingInSemester + " hours during the semester." );
+        System.out.println("That is " + String.format("%.2f", percentageOfCodingTime) + "% of the regular working time in the period.");
+    }
 }
