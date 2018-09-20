d8d971851611a55c602fe0586372de525947af85
Honza Dudek <honza@honzadudek.cz>
2018-09-20 17:26:41 +0200

    29th excercise

--- week-01/day-04/src/excercise29.java
+++ week-01/day-04/src/excercise29.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class excercise29 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many floors should the pyramid have?");
+        int numberOfLines = scanner.nextInt();
+        String star = "*";
+        String space = " ";
+        StringBuilder str = new StringBuilder(star);
+
+        for (int i = 0; i < numberOfLines; i++) {
+            for (int j = numberOfLines - i; j > 0; j--) {
+                System.out.print(space);
+            }
+            for (int k = 0; k < i; k++) {
+                System.out.print(star + star);
+            }
+            System.out.println(star);
+        }
+    }
 }
