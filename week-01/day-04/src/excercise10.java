ea6578daa55fc9282aa6fc64a0d761f60630b78e
Honza Dudek <honza@honzadudek.cz>
2018-09-20 13:21:15 +0200

    10th excercise

---
+++ week-01/day-04/src/excercise10.java
@@ -0,0 +1,13 @@
+public class excercise10 {
+    public static void main(String[] args) {
+        String myName = "Honza Dudek";
+        int age = 27;
+        double height = 1.86;
+        boolean married = true;
+
+        System.out.println("My name is " + myName + ".");
+        System.out.println("I am " + age + " years old.");
+        System.out.println("I am " + height + " meters tall.");
+        System.out.println("My marriage status is: " + married + ".");
+    }
+}
