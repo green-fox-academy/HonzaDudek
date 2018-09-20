4943e689db6fce884c28a023f1ace48d2225bd04
Honza Dudek <honza@honzadudek.cz>
2018-09-20 11:29:56 +0200

    5th excercise

---
+++ week-01/day-04/src/Excercise05.java
@@ -0,0 +1,14 @@
+public class Excercise05 {
+    public static void main(String[] args) {
+        int x = 22;
+        int y = 13;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(22 / 13.);
+        System.out.println(x / y);
+        System.out.println(x % y);
+
+    }
+}
