1a09c43e0bb6227f8da56fb360644090a4d2a408
Honza Dudek <honza@honzadudek.cz>
2018-09-20 11:55:25 +0200

    7th excercise

---
+++ week-01/day-04/src/Excercise07.java
@@ -0,0 +1,6 @@
+public class Excercise07 {
+    public static void main(String[] args) {
+        int myFavoriteNumber = 13;
+        System.out.println("My favorite number is: " + myFavoriteNumber);
+    }
+}
