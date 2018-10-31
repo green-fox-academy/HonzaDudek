77b222657e978b924e010d5f52bee862a61a4c6d
Honza Dudek <honza@honzadudek.cz>
2018-10-31 10:15:42 +0100

    Week-07 day-03 initial commit

--- week-07/day-01/src/Excercise10.java
+++ week-07/day-01/src/Excercise10.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Excercise10 {
+
+    public static List<Fox> foxes = new ArrayList<>();
+
+    public static void main(String[] args) {
+        foxes.add(new Fox("Karel", "pallida", "white"));
+        foxes.add(new Fox("Adam", "velox", "green"));
+        foxes.add(new Fox("Franta", "pallida", "green"));
+        foxes.add(new Fox("Bob", "velox", "brown"));
+        foxes.add(new Fox("Frank", "pallida", "green"));
+
+        foxes.stream()
+                .filter(fox -> fox.type.equals("pallida") && fox.color.equals("green"))
+                .forEach(fox -> System.out.println(fox.name));
+    }
+
 }
--- week-07/day-01/src/Fox.java
+++ week-07/day-01/src/Fox.java
@@ -1,2 +1,11 @@
 public class Fox {
+    String name;
+    String type;
+    String color;
+
+    public Fox(String name, String type, String color) {
+        this.name = name;
+        this.type = type;
+        this.color = color;
+    }
 }
