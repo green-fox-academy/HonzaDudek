3e8805389a4b48905e4a9e0c7792b273102dff04
Honza Dudek <honza@honzadudek.cz>
2018-11-17 10:26:45 +0100

    Database and main method created, tested server connection

--- Wekk-09/Reddit/src/main/java/com/greenfoxacademy/reddit/Models/Reddit.java
+++ Wekk-09/Reddit/src/main/java/com/greenfoxacademy/reddit/Models/Reddit.java
@@ -1,7 +1,13 @@
 package com.greenfoxacademy.reddit.Models;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Reddit {
 
-
+    public static void main(String[] args) {
+        SpringApplication.run(Reddit.class, args);
+    }
 
 }
