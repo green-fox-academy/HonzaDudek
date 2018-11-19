69eacc0d1ff3163255373d414d56ba7edbf58e5a
Honza Dudek <honza@honzadudek.cz>
2018-11-19 12:13:13 +0100

    Created Greeter class

--- Week-09/day-01/Frontend/src/main/java/com/greenfoxacademy/frontend/Models/Greeter.java
+++ Week-09/day-01/Frontend/src/main/java/com/greenfoxacademy/frontend/Models/Greeter.java
@@ -1,4 +1,30 @@
 package com.greenfoxacademy.frontend.Models;
 
 public class Greeter {
+    private String name;
+    private String title;
+
+    public Greeter() {
+    }
+
+    public Greeter(String name, String title) {
+        this.name = name;
+        this.title = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
