f5c16d1ee51ee0e8e54aa3cb322660db4788f013
Honza Dudek <honza@honzadudek.cz>
2018-11-22 15:17:23 +0100

    Created: Logic for creating new aliases, error and success messages

--- Week-09/Orientation-Exam-trial/src/main/java/com/greenfoxacademy/urlshortening/Controllers/MainController.java
+++ Week-09/Orientation-Exam-trial/src/main/java/com/greenfoxacademy/urlshortening/Controllers/MainController.java
@@ -6,8 +6,10 @@ import com.greenfoxacademy.urlshortening.Services.WebsiteServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
@@ -19,15 +21,24 @@ public class MainController {
         this.websiteServices = websiteServices;
     }
 
+    @GetMapping("/")
+    public String homepage() {
+        return "index";
+    }
+
     @PostMapping("/save-link")
     public String shortenURL(@RequestParam("url") String url,
-                      @RequestParam("alias") String alias, Model model) {
-        if (websiteServices.createShortenedURL(url, alias) != null) {
-            Website website = websiteServices.createShortenedURL(url, alias);
+                             @RequestParam("alias") String alias, Model model, RedirectAttributes attributes) {
+        Website website = websiteServices.createShortenedURL(url, alias);
+
+        if (website!= null) {
             model.addAttribute("website", website);
+            attributes.addAttribute("website", website);
             return "index";
         } else {
             model.addAttribute("error", "Your alias is already in use!!!");
+            attributes.addAttribute("error", "Your alias is already in use!!!");
+
             return "index";
         }
     }
